db1f73694dfdb497f7e9badb9c162b8653b00d07
Hiram Chirino <hiram@hiramchirino.com>
2015-08-27 14:27:30 -0400

    Update lic.

--- src/test/java/org/jboss/hawt/log4j/SanitizingPatternLayoutTest.java
+++ src/test/java/org/jboss/hawt/log4j/SanitizingPatternLayoutTest.java
@@ -1,20 +1,18 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2015 Red Hat, Inc.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
+ *
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
-
 package org.jboss.hawt.log4j;
 
 import org.apache.log4j.Logger;
@@ -41,7 +39,7 @@ public class SanitizingPatternLayoutTest {
         props.setProperty("log4j.appender.out.layout.replaceRegex","\\n");
         props.setProperty("log4j.appender.out.layout.replacement","--NL--");
 
-        props.setProperty("log4j.appender.out.layout.trim","true");
+        props.setProperty("log4j.appender.out.layout.trim", "true");
         PropertyConfigurator.configure(props);
         FOO.info("Hello\nWorld\n");
         assertEquals("INFO | [Hello--NL--World]", MockAppender.writer.toString());
